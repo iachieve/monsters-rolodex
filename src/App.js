092125cabdcbd5fe9a24e857ea8e0d023e1b8a61
iachieve <mhalim80@hotmail.com>
2020-04-14 20:31:24 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=> res.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map((mon, i) => <h1 key={mon.id}>{mon.name}</h1>)
+        }
+      </div>
+
+    );
+  }
+}
+
+export default App;
