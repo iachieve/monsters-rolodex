5a5ae672bddf5dd6a298aadfdaf43a6788de788e
iachieve <mhalim80@hotmail.com>
2020-04-14 23:01:51 -0700

    update design

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './card.styles.css';
 
 export const Card = ({ monster }) => {
+  let imgSets = ['set_set1', 'set_set2', 'set_set3', 'set_set4']
   return (
     <div className="card-container">
       <img alt="monster"
-        src={`https://robohash.org/set_set3/bgset_bg1//${monster.id}?size=180x180`} />
+        src={`https://robohash.org/${imgSets[Math.floor(Math.random() * 4)]}/bgset_bg1//${monster.id}?size=180x180`} />
       <h2>{monster.name}</h2>
       <p>{monster.email}</p>
     </div>
