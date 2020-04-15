3e139e431ba5c8c6771b991140e7cc723e82088d
iachieve <mhalim80@hotmail.com>
2020-04-14 22:35:52 -0700

    add search component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,7 +6,7 @@ export const Card = ({ monster }) => {
     <div className="card-container">
       <img alt="monster"
         src={`https://robohash.org/set_set3/bgset_bg1//${monster.id}?size=180x180`} />
-      <h1>{monster.name}</h1>
+      <h2>{monster.name}</h2>
       <p>{monster.email}</p>
     </div>
   )
